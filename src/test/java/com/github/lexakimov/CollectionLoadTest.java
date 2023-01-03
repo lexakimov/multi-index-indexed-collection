0a3dffe005406b29bcc42759e1d54c053f2fb57c
Alexey Akimov <lex.akimov23@gmail.com>
2023-01-03 22:42:57 +0700

    refactor unit tests

--- src/test/java/com/github/lexakimov/CollectionLoadTest.java
+++ src/test/java/com/github/lexakimov/CollectionLoadTest.java
@@ -5,75 +5,69 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.LinkedList;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import static com.github.lexakimov.PersonSearchableProperty.FIRST_NAME;
 import static com.github.lexakimov.PersonSearchableProperty.LAST_NAME;
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class CollectionLoadTest {
 
     @Test
     void searchInHugeCollection() throws IOException, URISyntaxException {
-
         var csvFilePath = Paths.get(getClass().getClassLoader().getResource("500_000_random_names.csv").toURI());
-
         var plainList = new LinkedList<Person>();
 
-        // parse csv file
-        try (var lines = Files.lines(csvFilePath)) {
-            lines
-                    .map(name -> name.split(" ", 2))
-                    .map(splitName -> new Person(splitName[0], splitName[1], 10))
-                    //.limit(10_000)
-                    .forEach(plainList::add);
+        {
+            var start = System.currentTimeMillis();
+            for (int i = 0; i < 30; i++) {
+                try (var lines = Files.lines(csvFilePath)) {
+                    lines
+                            .map(name -> name.split(" ", 2))
+                            .map(splitName -> new Person(splitName[0], splitName[1], 10))
+                            .forEach(plainList::add);
+                }
+            }
+            out.printf("%s elements parsed from CSV for %sms%n", plainList.size(), System.currentTimeMillis() - start);
+            out.println();
         }
 
-        var start1 = System.currentTimeMillis();
         var uut = new Collection<>(PersonSearchableProperty.class);
-        plainList.forEach(uut::addElement);
-        System.out.printf("%s elements added to collection for %s%n", uut.size(),
-                Duration.ofMillis(System.currentTimeMillis() - start1));
-
+        {
+            var start = System.currentTimeMillis();
+            plainList.forEach(uut::addElement);
+            out.printf("%s elements added to collection for %sms%n", uut.size(), System.currentTimeMillis() - start);
+            out.println();
+        }
 
         performSearchInCollection(uut, FIRST_NAME, "Kristin");
-        performLinearSearch(plainList, Person::firstName, "Kristin");
-        System.out.println();
-
-        performLinearSearch(plainList, Person::firstName, "Johnny");
         performSearchInCollection(uut, FIRST_NAME, "Johnny");
-        System.out.println();
-
-
-        performLinearSearch(plainList, Person::lastName, "Ruiz");
         performSearchInCollection(uut, LAST_NAME, "Ruiz");
-        System.out.println();
-
         performSearchInCollection(uut, LAST_NAME, "Castillo");
-        performLinearSearch(plainList, Person::lastName, "Castillo");
-        System.out.println();
-
         performSearchInCollection(uut, FIRST_NAME, "Miller");
-        performLinearSearch(plainList, Person::firstName, "Miller");
-        System.out.println();
-
         performSearchInCollection(uut, FIRST_NAME, "Steven");
-        performLinearSearch(plainList, Person::firstName, "Steven");
-        System.out.println();
+        out.println();
+
+        performLinearSearch(plainList, FIRST_NAME, "Kristin");
+        performLinearSearch(plainList, FIRST_NAME, "Johnny");
+        performLinearSearch(plainList, LAST_NAME, "Ruiz");
+        performLinearSearch(plainList, LAST_NAME, "Castillo");
+        performLinearSearch(plainList, FIRST_NAME, "Miller");
+        performLinearSearch(plainList, FIRST_NAME, "Steven");
+        out.println();
     }
 
     private static void performLinearSearch(
             LinkedList<Person> plainList,
-            Function<Person, String> getterMethod,
+            PersonSearchableProperty property,
             String value
     ) {
         var start = System.currentTimeMillis();
         var result = plainList.stream()
-                .filter(person -> getterMethod.apply(person).equals(value))
+                .filter(person -> property.getFunc().apply(person).equals(value))
                 .collect(Collectors.toCollection(LinkedList::new));
-        System.out.printf("[%-10s] %-5s elements found in list for %15sms%n",
+        out.printf("[%-10s] %-5s elements found in list for %15sms%n",
                 value, result.size(), System.currentTimeMillis() - start);
     }
 
@@ -84,7 +78,7 @@ class CollectionLoadTest {
     ) {
         var start = System.currentTimeMillis();
         var result = assertDoesNotThrow(() -> uut.searchByProperty(property, value));
-        System.out.printf("[%-10s] %-5s elements found in collection for %9sms%n",
+        out.printf("[%-10s] %-5s elements found in collection for %9sms%n",
                 value, result.size(), System.currentTimeMillis() - start);
     }
 
